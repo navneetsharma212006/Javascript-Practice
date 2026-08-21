37b18c3dba31b2dea8983789104c19e422a85bbf
navneetsharma212006 <navneetsh21@gmail.com>
2026-08-21 20:58:43 +0530

    update

---
+++ conditional.js
@@ -0,0 +1,3 @@
+/*
+we use conditional.js to handle conditional logic in our application. This file contains functions and utilities that allow us to evaluate conditions and execute code based on those evaluations. It helps in making decisions within the application flow, ensuring that the right actions are taken based on specific criteria.
+*/
